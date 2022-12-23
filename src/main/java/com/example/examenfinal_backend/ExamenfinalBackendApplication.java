2aa22d6acb802e1f4906ef614f41a0e801260e6b
Hael1230 <102492080+Hael1230@users.noreply.github.com>
2022-12-23 10:27:49 -0500

    Primer commit

---
+++ src/main/java/com/example/examenfinal_backend/ExamenfinalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.examenfinal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenfinalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenfinalBackendApplication.class, args);
+	}
+
+}
